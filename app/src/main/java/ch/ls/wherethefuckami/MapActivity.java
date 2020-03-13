8c4c8e42481589f14b31a0cedaddf413f5a823e6
Joël Wäger <jolwge@az.zli.ch>
2020-03-13 11:37:25 +0100

    added laoding img

--- app/src/main/java/ch/ls/wherethefuckami/MapActivity.java
+++ app/src/main/java/ch/ls/wherethefuckami/MapActivity.java
@@ -1,6 +1,7 @@
 package ch.ls.wherethefuckami;
 
 import android.Manifest;
+import android.app.ActionBar;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -37,6 +38,8 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getActionBar().setLogo(R.drawable.logo);
+        getActionBar().setDisplayUseLogoEnabled(true);
         super.onCreate(savedInstanceState);
         locations.add(new Information(0,0, 0));
         setContentView(R.layout.activity_maps);
@@ -57,6 +60,8 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             startService();
         }
     }
+
+
 
     void startService() {
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
